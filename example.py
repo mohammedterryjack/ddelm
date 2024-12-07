45d1f366ac89f89410db26d17f17207cb7fb4608
Mohammed Terry-Jack <mohammed@wluper.com>
2024-12-07 12:54:56 +0000

    image plots added

--- example.py
+++ example.py
@@ -1,19 +1,23 @@
+from matplotlib.pyplot import subplots, show
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_breast_cancer, load_iris
+from sklearn.datasets import load_breast_cancer, load_diabetes
 
 from src.utils import Activation, inverse_activation
 from src.elm import ELM
 from src.delm import DELM
 
-for load_data in (load_breast_cancer, load_iris):
+
+for load_data in (load_breast_cancer,):#, load_diabetes):
     data = load_data()
-    X_train, X_test, Y_train, Y_test = train_test_split(data.data, data.target, test_size=0.5, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(data.data, data.target, test_size=0.1, random_state=42)
 
-    _, d_i = data.data.shape
-    d_o = max(data.target) + 1
+    fig, axes = subplots(len(Activation), 5, figsize=(15, 5)) 
+    for j,a in enumerate(Activation): 
 
-    for a in (Activation.SINE, Activation.RELU):
+        _, d_i = data.data.shape
+        d_o = max(data.target) + 1
+
         elm = ELM(
             input_dimension=d_i, output_dimension=d_o, 
             hidden_dimension=100,
@@ -28,11 +32,20 @@ for load_data in (load_breast_cancer, load_iris):
         elm.fit(X=X_train, Y=Y_train)
         delm.fit(X=X_train, Y=Y_train)
 
+        for i, W in enumerate(delm.Ws):
+            ax = axes[j,i]
+            ax.imshow(W)  
+            if i==0:
+                ax.set_ylabel(a.name)
+            if j==len(Activation)-1:
+                ax.set_xlabel(f"Layer {i}")
+        
         Y_elm = elm.predict(X=X_test)
         Y_delm = delm.predict(X=X_test)
 
         accuracy_elm = accuracy_score(Y_test, Y_elm)
         accuracy_delm = accuracy_score(Y_test, Y_delm)
         print(
-            f"\n\nDataset: {data.filename}\nActivation: {a.name}\nAccuracy: \n\tELM:{accuracy_elm * 100:.2f}%\n\tDELM:{accuracy_delm * 100:.2f}%"
+            f"\n\nDataset: {data.filename} (d_i={d_i}, d_o={d_o})\nActivation: {a.name}\nAccuracy: \n\tELM:{accuracy_elm * 100:.2f}%\n\tDELM:{accuracy_delm * 100:.2f}%"
         )
+    show()
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,39 @@
 from enum import Enum
 
-from numpy import ndarray, zeros, maximum, sin,arcsin,nan_to_num
+from numpy import (
+    ndarray, 
+    zeros, 
+    maximum, 
+    sin,
+    arcsin,
+    nan_to_num,
+    cos,
+    arccos,
+    tan,
+    arctan
+)
 
 class Activation(Enum):
     SINE="sine"
+    COSINE = "cosine"
+    TAN = "tan"
     RELU="relu" 
 
 def activation_function(activation:Activation) -> callable:
     return {
         Activation.SINE:sin,
+        Activation.COSINE:cos,
+        Activation.TAN:tan,
         Activation.RELU:lambda x:maximum(0, x)
     }[activation]
 
 def inverse_activation(activation:Activation) -> callable:
-    return {
-        Activation.SINE:lambda x:nan_to_num(arcsin(x),nan=0.0),
+    f = {
+        Activation.SINE:arcsin,
+        Activation.COSINE:arccos,
+        Activation.TAN:arctan,
     }.get(activation, lambda x:x)
+    return lambda x:nan_to_num(f(x),nan=0.0)
 
 def one_hot_encode(class_ids: list[int], n_classes: int) -> ndarray:
     n_samples = len(class_ids)

ebb9c3215492b07f89813717ea131af053140243
Mohammed Terry-Jack <mohammed@wluper.com>
2024-12-08 02:19:25 +0000

    new graph

--- experiment_elm_vs_delm.py
+++ experiment_elm_vs_delm.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_breast_cancer
-from matplotlib.pyplot import subplots, show
+from matplotlib.pyplot import subplots, show, scatter, legend, xlabel, ylabel
 from sklearn.metrics import accuracy_score
 
 from src.utils import Activation
@@ -45,4 +45,36 @@ for i, (W_elm,W_delm) in enumerate(zip([elm.R,elm.W],delm.Ws)):
     axes[0,i].imshow(W_elm)  
     axes[1,i].imshow(W_delm)  
     axes[1,i].set_xlabel(f"W {i}")               
+show()
+
+
+accuracies_elm = []
+accuracies_delm = []
+h_dims = list(range(10,100,10))
+for h_dim in h_dims:
+    elm = ELM(
+        input_dimension=d_i, output_dimension=d_o, 
+        hidden_dimension=h_dim,
+        activation=Activation.SIN
+    )
+    delm = DELM(
+        input_dimension=d_i, output_dimension=d_o, 
+        hidden_dimensions=[h_dim],
+        activation=Activation.SIN
+    )
+
+    elm.fit(X=X, Y=Y)
+    delm.fit(X=X, Y=Y)
+
+    Y_elm = elm.predict(X=X)
+    Y_delm = delm.predict(X=X)
+
+    accuracies_elm.append(accuracy_score(Y, Y_elm))
+    accuracies_delm.append(accuracy_score(Y, Y_delm))
+
+scatter(h_dims,accuracies_elm,label='ELM')
+scatter(h_dims,accuracies_delm,label='DELM')
+ylabel('accuracy')
+xlabel('hidden layer dimension')
+legend()
 show()

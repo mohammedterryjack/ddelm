18e4412bd7041c55edf15a7a2da3bf2efe6bc1e7
Mohammed Terry-Jack <mohammed@wluper.com>
2024-12-08 13:35:02 +0000

    update visualisations

--- experiment_backward_pass.py
+++ experiment_backward_pass.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_breast_cancer
-from matplotlib.pyplot import imshow, show,title
+from matplotlib.pyplot import subplots, show
 
 from src.utils import Activation, one_hot_encode
 from src.delm import DELM
@@ -12,23 +12,52 @@ Y = data.target
 _, d_i = X.shape
 d_o = max(Y) + 1
 
+y = one_hot_encode(class_ids=Y, n_classes=d_o)
+
+X_test = X[:100]
+y_test = y[:100]
+
 for a in Activation:
+    if a != Activation.RELU:
+        continue
     delm = DELM(
         input_dimension=d_i, output_dimension=d_o, 
-        hidden_dimensions=[100,100],
-        activation=Activation.SIN
+        hidden_dimensions=[100],
+        activation=a
     )
-
     delm.fit(X=X, Y=Y)
 
-    y = one_hot_encode(class_ids=Y, n_classes=d_o)
+    y_hat = delm.forward(X=X_test,Ws=delm.Ws,activation=delm.activation)
+    _, axes = subplots(1, 3, figsize=(15, 5)) 
+    axes[0].imshow(y_test,cmap='pink')
+    axes[1].imshow(y_hat,cmap='pink')
+    axes[2].imshow(y_test-y_hat,cmap='pink')
+    axes[0].set_title('y (target)')
+    axes[1].set_title('y (predicted)')
+    axes[2].set_title('dy (error)')
+    show()
+
+    X_hat = delm.backward(Y=y_hat,Ws=delm.Ws,inverse_activation=delm.inverse_activation)
+    _, axes = subplots(1, 3, figsize=(15, 5)) 
+    axes[0].imshow(X_test,cmap='pink')
+    axes[1].imshow(X_hat,cmap='pink')
+    axes[2].imshow(X_test-X_hat,cmap='pink')
+    axes[0].set_title('X (given)')
+    axes[1].set_title('X (from y_hat)')
+    axes[2].set_title('dX (error)')
+    show()
+
     X_hat = delm.backward(
-        Y=y,
+        Y=y_test,
         Ws=delm.Ws,
         inverse_activation=delm.inverse_activation
     )
-    deltaX = X-X_hat
-
-    title(a.value)
-    imshow(deltaX > 1e-3)
+    _, axes = subplots(1, 3, figsize=(15, 5)) 
+    axes[0].imshow(X_test,cmap='pink')
+    axes[1].imshow(X_hat,cmap='pink')
+    axes[2].imshow(X_test-X_hat,cmap='pink')
+    axes[0].set_title('X (given)')
+    axes[1].set_title('X (from y)')
+    axes[2].set_title('dX (error)')
     show()
+

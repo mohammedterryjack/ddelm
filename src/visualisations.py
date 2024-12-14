0ca1785cead6c860758d986f3c4042c775f7d4dd
Mohammed Terry-Jack <mohammed@wluper.com>
2024-12-14 21:15:02 +0000

    3d

--- src/visualisations.py
+++ src/visualisations.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import subplots, show, annotate
+from matplotlib.pyplot import subplots, show, annotate, Normalize, cm
 from numpy import ndarray
 
 from src.cnn_delm import CNN
@@ -27,10 +27,10 @@ def display_forward_pass_cnn(model: CNN, X: ndarray, Y: ndarray) -> None:
         )
     )
 
-    _, axes = subplots(
+    n_cols = 3 * len(model.Wks) + 2 * len(model.Whs) + 1
+    fig, axes = subplots(
         2,
-        3 * len(model.Wks) + 2 * len(model.Whs) + 1,
-        # subplot_kw={"projection": "3d"},
+        n_cols,
         figsize=(15, 5),
     )
 
@@ -39,22 +39,39 @@ def display_forward_pass_cnn(model: CNN, X: ndarray, Y: ndarray) -> None:
     for i, W in enumerate(model.Wks + model.Whs):
         Y_hat = forward_pass(layer=i)
         is_cnn_layer = int(i < len(model.Wks))
-        if counter and is_cnn_layer:
-            axes[0, counter].imshow(Y_hat, cmap="pink")
-            counter += 1
+        if is_cnn_layer:
+            if counter:
+                axes[0, counter].imshow(Y_hat, cmap="pink")
+                #axes[1, counter].imshow(Y_hat, cmap="pink") #todo; change to 3d of transformed prev
+                counter += 1
         elif i > len(model.Wks):
             axes[1, counter].axis("off")
         axes[0, counter].imshow(Y_hat, cmap="pink")
+        if is_cnn_layer:
+            Y_hat_cnn = model.forward_pass_ff_to_cnn_layer(
+                ff_layer=Y_hat,
+                window_size=W.shape[0],
+                stride=model.stride,
+            )
+            norm = Normalize(Y_hat_cnn.min(), Y_hat_cnn.max())  
+            axes[1, counter] = fig.add_subplot(2, n_cols-1, n_cols+counter, projection='3d')  
+            axes[1, counter].voxels(Y_hat_cnn, facecolors=cm.viridis(norm(Y_hat_cnn))) 
         counter += 1
+        if is_cnn_layer:
+            axes[1, counter].imshow(W, cmap="inferno")
+            axes[0, counter].axis("off")
+        else:
+            axes[0, counter].imshow(W, cmap="inferno")
+            axes[1, counter].axis("off")
+        counter += 1
+
+
         # axes[0, j].set_title(
         #    f"Ŷ{SUBSCRIPTS[i]} = X"
         #    if j == 0
         #    else f"Ŷ{SUBSCRIPTS[i]} = H{SUBSCRIPTS[i]}"
         # )
         # axes[0, j + 1].voxels(W)
-        axes[int(not is_cnn_layer), counter].axis("off")
-        axes[is_cnn_layer, counter].imshow(W, cmap="inferno")
-        counter += 1
         # axes[is_cnn_layer, j + 1].set_title(
         #    "Wᵢₙ" if i == 0 else "Wₒᵤₜ" if i == n_layers - 1 else f"W{SUBSCRIPTS[i]}"
         # )

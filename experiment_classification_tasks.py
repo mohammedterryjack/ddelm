8cccab89e63ff42cf6f9c6aa61c5bf7bff8973a8
Mohammed Terry-Jack <mohammed@wluper.com>
2024-12-23 13:36:19 +0000

    x

--- experiment_classification_tasks.py
+++ experiment_classification_tasks.py
@@ -1,65 +1,17 @@
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import (
-    load_breast_cancer,
-    load_iris,
-    load_wine,
-    make_classification,
-    fetch_openml
-)
 from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from numpy import hstack, ndarray
-from matplotlib.pyplot import subplots, show
 
 from src.utils import Activation
 from src.elm import ELM
 from src.ffnn import FFNN
-
-def get_openml_dataset(name:str,version:int) -> tuple[ndarray,ndarray]:
-    scaler = StandardScaler()
-    onehot_encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
-
-    data = fetch_openml(name,version=version)
-    x = data.data
-    y = data.target
-
-    numerical_column_names = x.select_dtypes(include=['int64', 'float64']).columns.tolist()
-    categorical_column_names = x.select_dtypes(include=['object', 'category']).columns.tolist()
-    return (
-        hstack([scaler.fit_transform(x[numerical_column_names]), onehot_encoder.fit_transform(x[categorical_column_names])]),
-        y.cat.codes.to_numpy()
-    )
-
+from src.utils import TASKS
 
 a = Activation.RELU
-d_hs = [30,20]
-tasks = {
-    "synthetic":make_classification,
-    "breast cancer":lambda : (
-        load_breast_cancer().data,
-        load_breast_cancer().target 
-    ),
-    "iris":lambda :(
-        load_iris().data,
-        load_iris().target,
-    ),
-    "wine":lambda :(
-        load_wine().data,
-        load_wine().target
-    ),
-    "adult":lambda:get_openml_dataset(name="adult", version=2),
-    "diabetes":lambda:get_openml_dataset(name="diabetes", version=1),
-}
-
-_, axes = subplots(len(tasks),4,figsize=(15, 5))
-axes[0,0].set_title("Target")
-axes[0,1].set_title("ELM")
-axes[0,2].set_title("FFNN (novel)")
-axes[0,3].set_title("FFNN (backprop)")
+d_hs = [20,10]
 
 
-for i,(task_name,load_dataset) in enumerate(tasks.items()):
+for task_name,load_dataset in TASKS.items():
     X,y = load_dataset()
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, y, test_size=0.1, random_state=42
@@ -71,7 +23,7 @@ for i,(task_name,load_dataset) in enumerate(tasks.items()):
     elm = ELM(
         input_dimension=d_i,
         output_dimension=d_o,
-        hidden_dimension=sum(d_hs),
+        hidden_dimension=d_hs[0],
         activation=a,
     )
     ffnn = FFNN(
@@ -96,17 +48,5 @@ for i,(task_name,load_dataset) in enumerate(tasks.items()):
     accuracy_elm = accuracy_score(Y_test, Y_elm)
     accuracy_ffnn = accuracy_score(Y_test, Y_ffnn)
     accuracy_dnn = accuracy_score(Y_test, Y_dnn)
+
     print(f"Task: {task_name}\n\tELM:{accuracy_elm * 100:.2f}%\n\tFFNN:{accuracy_ffnn * 100:.2f}%\n\tDNN:{accuracy_dnn * 100:.2f}%")    
-
-    #axes[i,0].scatter(X_train[:,0], X_train[:, 1], c=Y_train)
-    #axes[i,1].scatter(X_train[:,0], X_train[:, 1], c=Y_train)
-    #axes[i,2].scatter(X_train[:,0], X_train[:, 1], c=Y_train)
-    #axes[i,3].scatter(X_train[:,0], X_train[:, 1], c=Y_train)
-    
-    axes[i,0].scatter(X_test[:, 0], X_test[:, 1], c=Y_test, alpha=0.6)
-    axes[i,1].scatter(X_test[:, 0],X_test[:, 1],c=Y_elm,alpha=0.6)
-    axes[i,2].scatter(X_test[:, 0],X_test[:, 1],c=Y_ffnn,alpha=0.6)
-    axes[i,3].scatter(X_test[:, 0],X_test[:, 1],c=Y_dnn,alpha=0.6)
-
-
-show()
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 from enum import Enum
+from sklearn.datasets import (
+    load_breast_cancer,
+    load_iris,
+    load_wine,
+    make_classification,
+    fetch_openml
+)
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from numpy import (
     ndarray,
@@ -7,6 +15,7 @@ from numpy import (
     nan_to_num,
     tan,
     arctan,
+    hstack
 )
 
 
@@ -36,3 +45,39 @@ def one_hot_encode(class_ids: list[int], n_classes: int) -> ndarray:
     vectors = zeros(shape=(n_samples, n_classes), dtype=int)
     vectors[range(n_samples), class_ids] = 1
     return vectors
+
+
+
+def get_openml_dataset(name:str,version:int) -> tuple[ndarray,ndarray]:
+    scaler = StandardScaler()
+    onehot_encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+
+    data = fetch_openml(name,version=version)
+    x = data.data
+    y = data.target
+
+    numerical_column_names = x.select_dtypes(include=['int64', 'float64']).columns.tolist()
+    categorical_column_names = x.select_dtypes(include=['object', 'category']).columns.tolist()
+    return (
+        hstack([scaler.fit_transform(x[numerical_column_names]), onehot_encoder.fit_transform(x[categorical_column_names])]),
+        y.cat.codes.to_numpy()
+    )
+
+def get_sklearn_dataset(name:str) -> tuple[ndarray,ndarray]:
+    data = {
+        "iris":load_iris, 
+        "breast_cancer":load_breast_cancer, 
+        "wine":load_wine
+    }[name]()
+    x=data.data
+    y=data.target
+    return x,y
+
+TASKS = {
+    "synthetic":make_classification,
+    "breast_cancer":lambda : get_sklearn_dataset(name="breast_cancer"),
+    "iris":lambda :get_sklearn_dataset(name="iris"),
+    "wine":lambda :get_sklearn_dataset(name="wine"),
+    "adult":lambda:get_openml_dataset(name="adult", version=2),
+    "diabetes":lambda:get_openml_dataset(name="diabetes", version=1),
+}
